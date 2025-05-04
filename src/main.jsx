94b99f88bfe5c5cb23d2e41fd4f378911042e050
allae <ibrahimallae1@gmail.com>
2025-05-04 23:04:18 +0200

    ajout des pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client"; // Import createRoot
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
+import App from "./App";
+import './index.css'; // Assurez-vous d'importer votre fichier CSS si nécessaire
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/site_projet8/',
-
-  root: '.', // or wherever your index.html is
+  base: '/site_projet8/',  // Assure-toi que ce chemin est correct pour ton dépôt GitHub
+  // root: '.',               // Si index.html est à la racine
 });
 
 
