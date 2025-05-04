889718567dc2ae01c65ad11e385c17b5ff9a56cd
allae <ibrahimallae1@gmail.com>
2025-05-04 16:22:29 +0200

    ajout du root

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/site_projet8/', // <- mets ici le nom de ton dépôt GitHub
+  root: '.', // or wherever your index.html is
 });
 
