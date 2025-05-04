084a2c7a04aad85dd76ba8c590ee0f1e25e0e2fa
allae <ibrahimallae1@gmail.com>
2025-05-04 16:26:23 +0200

    changement de base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/site_projet8/', // <- mets ici le nom de ton dépôt GitHub
+  base: '/', 
   root: '.', // or wherever your index.html is
 });
 
